8337ffe9009194945005e5679f151bb332fb8be5
Lucas Laurindo dos Santos <lucas.laurindo@jimdo.com>
2020-11-21 15:56:07 +0100

    Using env var port

--- server/index.js
+++ server/index.js
@@ -2,17 +2,18 @@ const express = require("express");
 const path = require('path');
 const app = express(); // create express app
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/backend/players',  (req, res) => {
     res.json("a")
 });
 
-app.get('/*', function (req, res) {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
-// start express server on port 5000
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log("server started on port 5000");
 });
