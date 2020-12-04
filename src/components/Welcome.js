c765774a7a1bc1299baa2e256661f7da8c8c615e
nicolearantes <nicolearantes@users.noreply.github.com>
2020-12-04 14:30:41 +0100

    Set Welcome style

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom'
 import Cookie from 'js-cookie'
+import './Welcome.css'
 
 import createGame from '../useCases/createGame.js'
 import joinGame from '../useCases/joinGame.js'
@@ -15,6 +16,8 @@ function Welcome(props) {
     function handleChange(event) {
         setPlayerName(event.target.value)
     }
+
+    
 
     async function handleClick() {
         const trimmedName = playerName.trim()
@@ -46,11 +49,17 @@ function Welcome(props) {
     }
 
     return (
-        <div>
-            <p>Your name</p>
-            <input type="text" value={playerName} onChange={handleChange} />
+        <div className="components-body">
+            <h1 className="blink-text"> Guess Game </h1>
+            <h2>Welcome!</h2>
+            <p>{!gameId ? "Please write your name down to create a new game." : "Please write your name down to join the game."}</p>
+            <input 
+            type="text" 
+            value={playerName} 
+            placeholder = "Your name or nickname"
+            onChange={handleChange} />
 
-            <button onClick={handleClick}>Start</button>
+            <button type="submit" onClick={handleClick}>{!gameId ? "Create Game" : "Join Game"}</button>
         </div>
     );
 }
