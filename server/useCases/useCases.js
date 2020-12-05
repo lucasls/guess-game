b9f903719efce787a54123d55c3ec3fb7b8d5004
Lucas Laurindo dos Santos <lucas.laurindo@jimdo.com>
2020-12-05 13:35:39 +0100

    Set game state to playing and return team info at findGame

--- server/useCases/useCases.js
+++ server/useCases/useCases.js
@@ -1,6 +1,31 @@
 
 const repository = require('../persistence/repository')
 const { v4: uuidv4 } = require('uuid');
+const GameState = require('../domain/GameState');
+const groupArray = require('group-array');
+const Team = require('../domain/Team');
+
+function getTurnPlayer(playersByTeam, turn) {
+    const team = turn % 2 === 0 ? Team.GREEN : Team.BLUE
+    const teamPlayers = playersByTeam[team]
+
+    return teamPlayers[Math.floor(turn / 2) % teamPlayers.length]
+}
+
+exports.findGame = async function(gameId) {
+    const game = await repository.findGame(gameId)
+
+    if (!game) {
+        return null
+    }
+
+    game.playersByTeam = groupArray(game.players, p => p.team)
+    
+    game.currentPlayer = getTurnPlayer(game.playersByTeam, game.currentTurn)
+    game.nextPlayer = getTurnPlayer(game.playersByTeam, game.currentTurn + 1)
+
+    return game
+}
 
 exports.addWords = async function (gameId, playerId, words) {
     await repository.addWords(gameId, words.map(content => ({
@@ -8,6 +33,12 @@ exports.addWords = async function (gameId, playerId, words) {
         playerId: playerId,
         content: content
     })))
+
+    const allPlayersDone = (await repository.findPlayersWithoutWords(gameId)).length === 0
+
+    if (allPlayersDone) {
+        repository.setGameState(gameId, GameState.PLAYING)
+    }
 }
 
 exports.findPlayerWords = async function (gameId, playerId) {
