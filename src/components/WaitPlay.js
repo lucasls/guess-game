957495905ef624fdc202d3adaca2447a887c47ae
Lucas Laurindo dos Santos <lucas.laurindo@jimdo.com>
2020-12-05 14:24:08 +0100

    Make WaitPlay work

--- src/components/WaitPlay.js
+++ src/components/WaitPlay.js
@@ -1,83 +1,96 @@
-import React, { useState } from 'react';
+import delay from 'delay';
+import React, { useEffect, useState } from 'react';
 import './WaitPlay.css'
 
 
 function WaitPlay(props) {
-    // const game = props.gameData.game
-    // const playerNow = props.gameData.game.players[0]
-    // const playerId = props.gameData.playerId
-    // const phase = props.gameData.currentPhase
-    // const [round, setRound]= useState()
-    
-    // function instructions() {
-    //     let phaseText
-    //     if(phase === 0) {
-    //         setTimeout(playerAndTeam, 3000)
-    //         phaseText = "One player receives one word/expression and have to explain for the team without saiyng exacly the word neither use synonyms;"
-    //     }else if(phase === 2) {
-    //                 setTimeout(playerAndTeam, 3000)
-    //         phaseText = "One player receives one word/expression and have to explain for the team with just ONE word"
-    //     } else if(phase === 3) {
-    //                 setTimeout(playerAndTeam, 3000)
-    //         phaseText = "One player receives one word/expression and have to explain for the team by mimicry"
-    //     }    
-    //     return <div>
-    //             <p>{phaseText}</p>
-    //             <p>The player's team have to hit the word by writing it in the input box;</p>
-    //             <p>When the answer is right the player receive a new world while there's time!</p>
-    //         </div>
-    // }
+    const game = props.gameData.game
+    const playerId = props.gameData.playerId
+    const player = game.players.find(player => player.id === playerId)
 
-    // function handleChange(event) {
-    //     event.targer.value
-    // }
+    const [instructionsTimer, setInstructionsTimer] = useState(7)
+    const phase = game.currentPhase
 
-    // function playerAndTeam() {
+    useEffect(
+        async () => {
+            
+            let timer = instructionsTimer
+            while(timer > 0) {
+                await delay(1000)
+                timer--
+                setInstructionsTimer(timer)
+            }
 
-    //     if(round === instructions) {
-    //         instruction()
-    //     }
-    //     if(round ===gamer) {
-    //         return <div>
-    //             {/* <p>{playWord}</p> */}
-    //             <button>Skip</button>
-    //         </div>
-    //     }
-    //     if(round === team) {
-    //         return <div>
-    //             {/* <input }></input> */}
-    //             <button> Send </button>
-    //         </div>
-    //     }
+        }, []
+    )
 
-    //     if(round === againstTime) {
-    //         return <div>
-    //             <p>It's {playerNow.team} turn, mute your mic please!</p>
-    //             <i class="fas fa-microphone-slash"></i>
-    //         </div>
-    //     }
+    function instructions() {
 
-    // }
+        let phaseText
+        switch (phase) {
+            case 0:
+                phaseText = "One player receives one word/expression and have to explain for the team without saiyng exacly the word neither use synonyms;"
+                break;
+            case 1:
+                phaseText = "One player receives one word/expression and have to explain for the team with just ONE word"
+                break;
+            case 2:
+                phaseText = "One player receives one word/expression and have to explain for the team by mimicry"
+                break;
+        }
 
-    // setRound(instructions)
+        return <div>
+            <p>{phaseText}</p>
+            <p>The player's team have to hit the word by writing it in the input box;</p>
+            <p>When the answer is right the player receive a new world while there's time!</p>
+            <p>{game.currentTurn === 0 ? "The first player will be" : "Who's playing now is"} {game.currentPlayer.name}</p>
+            <p>Game starts in {instructionsTimer}s</p>
+        </div>
+    }
 
-    // return <div className="components-body wait-play-component">
-        {/* <p>Green<span>{greenPoints}</span>x<span>{bluePoints}</span>Blue</p> */}
-        // <h1> Round {phase} </h1>
-        {/* <p>Remaining words: {remainingWords}</p> */}
+    function playerAndTeam() {
+        if (instructionsTimer > 0) {
+            return instructions()
+        }
 
-    //     {playerAndTeam}
+        if(game.currentPlayer.id === playerId) {
+            return <div>
+                {/* <p>{playWord}</p> */}
+                <button>Skip</button>
+            </div>
+        }
+        if(game.currentPlayer.team === player.team) {
+            return <div>
+                <input />
+                <button> Send </button>
+            </div>
+        }
 
-    //     <p>Now: {playerNow}</p>
-    //     <p>Next: {props.gameData.game.players[1]}</p>
+        
+        return <div>
+            <p>It's {game.currentPlayer.team.toLowerCase()} turn, mute your mic please!</p>
+            <i class="fas fa-microphone-slash"></i>
+        </div>    
 
-    // </div>
+    }
 
-    // console.log(props.allplayers)
+    const greenPoints = 0
+    const bluePoints = 0
+    const remainingWords = 10
 
-    return <div>
-        <p>Rolou...</p>
+
+    return <div className="components-body wait-play-component">
+        <p>Green <span>{greenPoints}</span> x <span>{bluePoints}</span> Blue</p>
+        <h1> Phase {phase + 1} </h1>
+        <p>Remaining words: {remainingWords}</p>
+
+        {playerAndTeam()}
+
+        <p>Now: {game.currentPlayer.name}</p>
+        <p>Next: {game.nextPlayer.name}</p>
+
     </div>
+
 }
 
 export default WaitPlay;
